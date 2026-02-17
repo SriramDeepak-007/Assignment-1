f08d377f79af2f72e6b1bcf97bea2e69f2df71ad
SriramDeepak-007 <sriramdeepaksrd@gmail.com>
2026-02-17 17:35:26 +0530

    Assignment-1

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+#problem statement: write a python program that does the following
+# 1.take two numbers as input from the user
+# 2.  Performs the basic mathematical operations on these two numbers: Addition	Subtraction Multiplication Division
+# 3. display the results of each operation on the screen
+
+Num1 = float(input("Enter the first number: "))
+Num2 = float(input("Enter the second number: "))
+
+Addition = Num1 + Num2
+Subtraction = Num1 - Num2
+Multiplication = Num1 * Num2
+Division = Num1 / Num2 
+
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
---
+++ Task2.py
@@ -0,0 +1,9 @@
+#Problem Statement: Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+#2.  Concatenates the first name and last name into a full name.
+#3.  Prints a personalized greeting message using the full name.
+
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+FullName = FirstName + " " + LastName
+print("Hello, " + FullName + "! Welcome to the Python programming world!")
